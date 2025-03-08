b42e76b57d39c929310f7b59b857e54f01d4fa02
imrat-67 <113079273+imrat-67@users.noreply.github.com>
2025-03-08 18:05:25 +0600

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,18 +7,18 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const platformUrls: Record<string, string> = {
-  'codeforces': 'https://codeforces.com/profile/imrat_67',
-  'atcoder': 'https://atcoder.jp/users/imrat_67',
-  'lightoj': 'https://lightoj.com/user/imrat_67',
-  'leetcode': 'https://leetcode.com/imrat_67/',
-  'cses': 'https://cses.fi/user/imrat_67',
-  'codechef': 'https://www.codechef.com/users/imrat_67',
-  'timus': 'https://acm.timus.ru/author.aspx?id=imrat_67',
-  'spoj': 'https://www.spoj.com/users/imrat_67/',
-  'hackerrank': 'https://www.hackerrank.com/imrat_67',
-  'hackerearth': 'https://www.hackerearth.com/@imrat_67',
-  'uva': 'https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_authorstats&userid=imrat_67',
-  'geeksforgeeks': 'https://auth.geeksforgeeks.org/user/imrat_67'
+  'codeforces': 'https://codeforces.com/profile/imrat_7',
+  'atcoder': 'https://atcoder.jp/users/imrat_7',
+  'lightoj': 'https://lightoj.com/user/imrat_6',
+  'leetcode': 'https://leetcode.com/imrat_7/',
+  'cses': 'https://cses.fi/user/imrat_7',
+  'codechef': 'https://www.codechef.com/users/imrat_7',
+  'timus': 'https://acm.timus.ru/author.aspx?id=imrat_7',
+  'spoj': 'https://www.spoj.com/users/imrat_7/',
+  'hackerrank': 'https://www.hackerrank.com/imrat_7',
+  'hackerearth': 'https://www.hackerearth.com/@imrat_7',
+  'uva': 'https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_authorstats&userid=imrat_7',
+  'geeksforgeeks': 'https://auth.geeksforgeeks.org/user/imrat_7'
 };
 
 export function numberWithCommas(x: number): string {
